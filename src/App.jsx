f0f7165d8b5966e1cd757786998546ad006c4368
Tanzin-Ul-Islam <tanzin.8897@gmail.com>
2023-02-20 02:14:48 +0600

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 import { Box } from '@mui/system'
 import { Navbar, Feed, VideoDetails, ChannelDetails, SearchFeed } from './component'
 export default function App() {
+  let location = useLocation();
   return (
-    <BrowserRouter>
 
-      <Box sx={{backgroundColor:'#000', pt:'10px'}}>
-        <Navbar />
-        <Routes>
-          <Route path="/" exact element={<Feed />} />
-          <Route path="/video/:id" exact element={<VideoDetails />} />
-          <Route path="/channel/:id" exact element={<ChannelDetails />} />
-          <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
-        </Routes>
-      </Box>
-
-    </BrowserRouter>
+    <Box sx={{ backgroundColor: '#000', pt: '10px' }}>
+      <Navbar />
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" key={location.pathname} exact element={<VideoDetails />} />
+        <Route path="/channel/:id" exact element={<ChannelDetails />} />
+        <Route path="/search/:searchTerm" exact element={<SearchFeed />} />
+      </Routes>
+    </Box>
   )
 }
--- src/component/Videos.jsx
+++ src/component/Videos.jsx
@@ -1,19 +1,19 @@
 import { Box, Stack } from '@mui/material';
 import React from 'react'
 import { VideoCard, ChannelCard } from "./";
-export default function Videos({ videos }) {
+export default function Videos({ videos, direction }) {
   return (
-    <Stack direction={'row'} flexWrap="wrap" justifyContent="start" gap={2}>
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" gap={2}>
       {
         videos && videos.length > 0 ?
-        videos.map((el, index) => {
-          return (
-            <Box key={index}>
-              {el.id.videoId && <VideoCard videoInfo={el} />}
-              {el.id.channelId && <ChannelCard channelDetail={el} />}
-            </Box>
-          )
-        }) : "No data found"
+          videos.map((el, index) => {
+            return (
+              <Box key={index}>
+                {el.id.videoId && <VideoCard videoInfo={el} />}
+                {el.id.channelId && <ChannelCard channelDetail={el} />}
+              </Box>
+            )
+          }) : "No data found"
       }
     </Stack>
   )
